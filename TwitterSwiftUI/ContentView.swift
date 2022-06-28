24a7744edf51d104f42198490c97b8f7792d8140
johnmccants002 <johnmccants002@gmail.com>
2022-06-27 20:26:38 -0700

    Initial Commit

---
+++ TwitterSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TwitterSwiftUI
+//
+//  Created by John McCants on 6/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
